39fa4d6c34a129a0f5674d00bb2e4ea389b0fbaf
Gagan <grampur123@gmail.com>
2023-12-04 16:07:06 -0800

    Added JUnit tests for PlaylistController

--- src/test/java/com/javatpoint/finalProject/finalProjectTests.java
+++ src/test/java/com/javatpoint/finalProject/finalProjectTests.java
@@ -1,4 +1,4 @@
-package com.javatpoint.springbootcrudoperation;
+package com.javatpoint.finalProject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
