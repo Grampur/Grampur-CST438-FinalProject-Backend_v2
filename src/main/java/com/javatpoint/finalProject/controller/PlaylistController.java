217318b585ba2c3e83a447cc94ded5b850855424
Gagan Rampur <grampur123@gmail.com>
2023-12-11 13:54:02 -0800

    Correctly calling the JWT credentials

--- src/main/java/com/javatpoint/finalProject/controller/PlaylistController.java
+++ src/main/java/com/javatpoint/finalProject/controller/PlaylistController.java
@@ -1,8 +1,11 @@
 
 package com.javatpoint.finalProject.controller;
 
+import java.security.Principal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -67,6 +70,16 @@ public class PlaylistController {
     @DeleteMapping ("/playlist/{playlistId}/song/{songId}")
     private void deleteSongFromPlaylist(@PathVariable("playlistId") int playlistId, @PathVariable("songId") int songId) {
     	playlistService.deleteSongFromPlaylist(playlistId, songId);
+    }
+    
+    
+    @GetMapping("/user/playlists")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<List<Playlist>> getUserPlaylists(Principal principal) {
+        // Assuming the User class has a method to get the username or ID
+        String username = principal.getName();
+        List<Playlist> playlists = playlistService.findByUserId(username);
+        return ResponseEntity.ok(playlists);
     }
     
     
--- src/main/java/com/javatpoint/finalProject/repository/PlaylistRepository.java
+++ src/main/java/com/javatpoint/finalProject/repository/PlaylistRepository.java
@@ -1,9 +1,11 @@
 package com.javatpoint.finalProject.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.javatpoint.finalProject.model.Playlist;
 
 public interface PlaylistRepository extends CrudRepository<Playlist, Integer> {
-
+    List<Playlist> findByUserid(String userid);
 }
